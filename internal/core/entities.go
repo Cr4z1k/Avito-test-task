376b0a9c71ca57ea278c3d526c91263070bd15d1
Cr4z1k <mr.kolya560@mail.ru>
2024-04-07 18:36:27 +0300

    added models for features and tags, also added entities for banner get end-pounts

--- internal/core/entities.go
+++ internal/core/entities.go
@@ -1,1 +1,17 @@
 package core
+
+type BannerContent struct {
+	Title string
+	Text  string
+	Url   string
+}
+
+type BannerWithFilters struct {
+	BannerID  uint64
+	TagIDs    []uint64
+	FeatureID uint64
+	Content   BannerContent
+	IsActive  bool
+	CreatedAt string
+	UpdatedAt string
+}
--- internal/core/models.go
+++ internal/core/models.go
@@ -1,8 +1,21 @@
 package core
 
+import "time"
+
 type Banner struct {
-	ID    uint64 `json:"id"`
-	Title string `json:"title"`
-	Text  string `json:"text"`
-	Url   string `json:"url"`
+	ID        uint64    `json:"id" db:"id"`
+	Title     string    `json:"title" db:"title"`
+	Text      string    `json:"text" db:"text"`
+	Url       string    `json:"url" db:"url"`
+	IsActive  bool      `json:"is_active" db:"is_active"`
+	CreatedAt time.Time `json:"created_at" db:"created_at"`
+	UpdatedAt time.Time `json:"updated_at" db:"updated_at"`
+}
+
+type Feature struct {
+	ID uint64 `json:"id" db:"id"`
+}
+
+type Tag struct {
+	ID uint64 `json:"id" db:"id"`
 }
